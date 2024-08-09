27aa03c548cf9f1bb4b6f2cd081dcfafb61a8ef8
silvinls <silvioluizs.santos@gmail.com>
2024-08-09 09:08:55 -0300

    tornando site responsivo

--- script/script.js
+++ script/script.js
@@ -1,15 +1,15 @@
 window.sr = ScrollReveal({ reset: true });
 
-sr.reveal('.cabecalho',{duration: 1000});
-sr.reveal('.secao1',{duration: 1000});
-sr.reveal('.secao2',{duration: 1000});
-sr.reveal('.secao3',{duration: 1000});
-sr.reveal('.secao4',{duration: 1000});
-sr.reveal('.secao5',{duration: 1000});
-sr.reveal('.secao6',{duration: 1000});
-sr.reveal('.secao7',{duration: 1000});
-sr.reveal('.secao8',{duration: 1000});
-sr.reveal('.secao9',{duration: 1000});
-sr.reveal('.secao10',{duration: 1000});
-sr.reveal('.rodape',{duration: 1000});
-sr.reveal('#faq',{duration: 1000})
+sr.reveal('.cabecalho',{duration: 500});
+sr.reveal('.secao1',{duration: 500});
+sr.reveal('.secao2',{duration: 500});
+sr.reveal('.secao3',{duration: 500});
+sr.reveal('.secao4',{duration: 500});
+sr.reveal('.secao5',{duration: 500});
+sr.reveal('.secao6',{duration: 500});
+sr.reveal('.secao7',{duration: 500});
+sr.reveal('.secao8',{duration: 500});
+sr.reveal('.secao9',{duration: 500});
+sr.reveal('.secao10',{duration: 500});
+sr.reveal('.rodape',{duration: 500});
+sr.reveal('#faq',{duration: 500})
